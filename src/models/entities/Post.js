168d73324632918e51a8e0f69e25f257d0c7c397
Lucifino <31148787+Lucifino@users.noreply.github.com>
2019-12-03 21:51:28 +0800

    Add files via upload

--- src/models/entities/Post.js
+++ src/models/entities/Post.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const REACTION_SCHEMA = require('../prerequisites/Reaction');
-const COMMENT_SCHEMA = require('../prerequisites/Comment');
+const COMMENT_SCHEMA= require('../prerequisites/Comment');
 
 const POST_SCHEMA = new Schema({
-  user_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'USER',
+  author: {
+    type: String,
+    required: true
+  },
+  destination_wall: {
+    type: String,
     required: true
   },
   content: {
@@ -15,8 +18,11 @@ const POST_SCHEMA = new Schema({
     required: true,
     default: '',
   },
-  reactions: [REACTION_SCHEMA],
-  comments: [COMMENT_SCHEMA],
+  comments: [{
+    type: Schema.Types.ObjectId,
+    ref: 'COMMENT',
+    required: false,
+  }],
 },
   {
     timestamps: true 
