2637bde8c8696f416b957142c06f8d991ec0d427
Lucifino <31148787+Lucifino@users.noreply.github.com>
2019-12-06 08:02:55 +0800

    Add files via upload

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const tokenBlacklist = []
 
 module.exports = {
   response: (success, message, data=null) => {
@@ -8,12 +9,15 @@ module.exports = {
   verifyToken: (req, res, next) => {
     //@ ONCE LOGIN IS FINISHED, UNCOMMENT
     const header_auth = req.headers['authorization'];
+
     bearer_token = header_auth.split(" ")
     API_TOKEN = bearer_token[1]
     
     if(!API_TOKEN){
       return res.send({success: false, message: 'Missing token header', data: null});
     }else{
+      console.log(tokenBlacklist);
+      if(tokenBlacklist.includes(API_TOKEN)) return res.send({success: false, message: `Token not valid`, data: null});
       jwt.verify(API_TOKEN, process.env.SECRET_KEY, (err, data) => {
         if(err) return res.send({success: false, message: `Token not valid`, data: null});
         else {
@@ -23,7 +27,22 @@ module.exports = {
         }
       })
     }
+  },
 
-    // next();
+  invalidateToken: (req, res) => {
+    const header_auth = req.headers['authorization'];
+
+    bearer_token = header_auth.split(" ")
+    API_TOKEN = bearer_token[1]
+    if(!API_TOKEN)return res.send({success: false, message: 'Missing token header', data: null});
+    jwt.verify(API_TOKEN, process.env.SECRET_KEY, (err, data) => {
+        if(err) return res.send({success: false, message: `Token not valid`, data: null});
+        else {
+          req.POST_VERIFICATION = null;
+          req.headers['authorization'] = null;
+          tokenBlacklist.push(API_TOKEN)
+          return res.send({success: true, message: `Succefully logged out!`, data: null})
+        }
+    })
   }
 }
